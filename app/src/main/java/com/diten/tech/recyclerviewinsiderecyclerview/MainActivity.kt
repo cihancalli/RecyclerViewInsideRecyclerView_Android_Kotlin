0a3584a4ca57fb37d101acdace5c489b92d5cfb7
cihancalli <cihan.callii@gmail.com>
2022-06-02 09:11:30 +0300

    adapter

--- app/src/main/java/com/diten/tech/recyclerviewinsiderecyclerview/MainActivity.kt
+++ app/src/main/java/com/diten/tech/recyclerviewinsiderecyclerview/MainActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.diten.tech.recyclerviewinsiderecyclerview.adapter.RVAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var rvAdapter: RVAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +23,8 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@MainActivity)
             val decoration = DividerItemDecoration(applicationContext,DividerItemDecoration.VERTICAL)
             addItemDecoration(decoration)
+            rvAdapter = RVAdapter()
+            adapter = rvAdapter
         }
     }
 }
--- app/src/main/java/com/diten/tech/recyclerviewinsiderecyclerview/adapter/RVAdapter.kt
+++ app/src/main/java/com/diten/tech/recyclerviewinsiderecyclerview/adapter/RVAdapter.kt
@@ -1,5 +1,28 @@
 package com.diten.tech.recyclerviewinsiderecyclerview.adapter
 
-class RVAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RVAdapter: RecyclerView.Adapter<RVAdapter.MyViewHolder>()  {
+
+
+
+    class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 
 }
