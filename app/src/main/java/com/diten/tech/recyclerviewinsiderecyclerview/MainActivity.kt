45296c0f0d7f5b732762e1cde6630a997fc2e839
cihancalli <cihan.callii@gmail.com>
2022-06-02 11:12:11 +0300

    Adapter 3

--- app/src/main/java/com/diten/tech/recyclerviewinsiderecyclerview/MainActivity.kt
+++ app/src/main/java/com/diten/tech/recyclerviewinsiderecyclerview/MainActivity.kt
@@ -7,11 +7,12 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.diten.tech.recyclerviewinsiderecyclerview.adapter.RVAdapter
+import com.diten.tech.recyclerviewinsiderecyclerview.model.LocationData
 import com.diten.tech.recyclerviewinsiderecyclerview.model.LocationList
 import com.diten.tech.recyclerviewinsiderecyclerview.viewModel.MainActivityViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , RVAdapter.OnRecyclerItemClick {
 
     lateinit var rvAdapter: RVAdapter
     lateinit var viewModel: MainActivityViewModel
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@MainActivity)
             val decoration = DividerItemDecoration(applicationContext,DividerItemDecoration.VERTICAL)
             addItemDecoration(decoration)
-            rvAdapter = RVAdapter()
+            rvAdapter = RVAdapter(this@MainActivity)
             adapter = rvAdapter
         }
     }
@@ -41,5 +42,9 @@ class MainActivity : AppCompatActivity() {
             rvAdapter.notifyDataSetChanged()
         })
         viewModel.loadData(this@MainActivity)
+    }
+
+    override fun onItemClikListener(data: LocationData) {
+
     }
 }
--- app/src/main/java/com/diten/tech/recyclerviewinsiderecyclerview/adapter/RVAdapter.kt
+++ app/src/main/java/com/diten/tech/recyclerviewinsiderecyclerview/adapter/RVAdapter.kt
@@ -2,20 +2,24 @@ package com.diten.tech.recyclerviewinsiderecyclerview.adapter
 
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.*
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.diten.tech.recyclerviewinsiderecyclerview.R
 import com.diten.tech.recyclerviewinsiderecyclerview.model.LocationData
 import kotlinx.android.synthetic.main.recycler_list_row.view.*
 
-class RVAdapter: RecyclerView.Adapter<RVAdapter.MyViewHolder>()  {
+class RVAdapter(val clickListener: OnRecyclerItemClick): RecyclerView.Adapter<RVAdapter.MyViewHolder>()  {
 
     var localListData = mutableListOf<LocationData>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val inflater = LayoutInflater.from(parent.context).inflate(R.layout.recycler_list_row,parent,false)
-        return MyViewHolder(inflater)
+        return MyViewHolder(inflater,clickListener)
     }
 
     override fun getItemCount(): Int {
@@ -24,16 +28,21 @@ class RVAdapter: RecyclerView.Adapter<RVAdapter.MyViewHolder>()  {
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.bind(localListData[position])
+        holder.itemView.setOnClickListener {
+            clickListener.onItemClikListener(localListData[position])
+        }
     }
 
-    class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
+    class MyViewHolder(view: View, val clickListener: OnRecyclerItemClick): RecyclerView.ViewHolder(view){
 
         val imageview = view.imageview
         val textViewName = view.textViewName
         val textViewAddress = view.textViewAddress
+        val tvChildCount = view.tvChildCount
+        val childRecyclerView  =view.childRecyclerView
 
         fun bind(data : LocationData) {
-            textViewName.text = data.locationName
+            //textViewName.text = data.locationName
             var address = ""
 
             data.address?.let {
@@ -55,11 +64,33 @@ class RVAdapter: RecyclerView.Adapter<RVAdapter.MyViewHolder>()  {
             data.country?.let {
                 address += it
             }
-            textViewAddress.text = address
+            //textViewAddress.text = address
 
             Glide.with(imageview).load(data.url).into(imageview)
 
+            if (data.childLocations != null && data.childLocations.size > 0){
+                tvChildCount.visibility = VISIBLE
+                childRecyclerView.visibility = VISIBLE
+                childRecyclerView.apply {
+                    //layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false)
+                    layoutManager = GridLayoutManager(context,3)
+                    val decoration = DividerItemDecoration(context,DividerItemDecoration.HORIZONTAL)
+                    addItemDecoration(decoration)
+                    val rvAdapter = RVAdapter(clickListener)
+                    rvAdapter.localListData = data.childLocations.toMutableList()
+                    adapter = rvAdapter
+                }
+                tvChildCount.text = "Child Location " + data.childLocations.size
+            }else{
+                tvChildCount.visibility = GONE
+                childRecyclerView.visibility = GONE
+            }
+
         }
+    }
+
+    interface OnRecyclerItemClick {
+        fun onItemClikListener(data : LocationData)
     }
 
 
